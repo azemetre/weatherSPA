e7b1c975d43ec03fb826a8aec16cc555bcdae419
Aaron Zemetres <azemetre@gmail.com>
2015-05-19 15:35:33 -0400

    Setting up paths and shims for require.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,30 @@
+require.config({
 
+    paths: {
+        'jquery': 'libs/jquery-1.11.2.min',
+        'underscore': 'libs/underscore-1.8.3.min',
+        'backbone': 'libs/backbone-1.2.0.min',
+        'bootstrap': 'libs'bootstrap'
+    },
+
+    shim: {
+        'underscore': {
+            exports: '_'
+        },
+        'backbone': {
+            deps: [
+                'underscore',
+                'jquery'
+            ],
+            exports: 'Backbone'
+        },
+        'bootstrap': {
+            deps: [
+                'jquery'
+            ]
+        }
+    }
+
+});
+
+require();
