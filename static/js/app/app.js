cca69bfaee4dacb45d1033ba4f323d2415b49538
Aaron Zemetres <azemetre@gmail.com>
2015-05-20 21:59:50 -0400

    Added AppModel and AppView and testing the wunderground API with example collection

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -1,15 +1,32 @@
 define([
     'app/views/app',
-    'app/routers/router'
-], function (AppView, Router) {
+    'app/routers/router',
+    'app/models/app',
+    'app/collections/days'
+], function (AppView, Router, AppModel, DaysCollection) {
     'use strict';
 
     var initialize = function () {
-        var appView = new AppView();
-        $('body').append(appView.el);
+        var appModel = new AppModel();
+
+        var appView = new AppView({model: appModel});
+        $('body').append(appView.render().el);
 
         var router = new Router(appView);
         Backbone.history.start();
+
+        var daysCollection = new DaysCollection([], {
+            url: 'http://api.wunderground.com/api/fee457a9f9b365ab/forecast/q/FL/Tampa.json'
+        });
+
+        daysCollection.fetch({
+            success: function(collection, response, options) {
+                console.log(collection, response);
+            },
+            error: function(collection, response, options) {
+                console.log('error');
+            }
+        });
     };
 
     return {
