ad69ddffd3250b8734fbb17d71d33e8805f43506
Aaron Zemetres <azemetre@gmail.com>
2015-05-24 02:40:16 -0400

    Initial commit

---
+++ static/js/app/views/modal.js
@@ -0,0 +1,38 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'app/templates',
+    'bootstrap'
+], function ($, _, Backbone, Templates, Bootstrap) {
+    'use strict';
+
+    var ModaelView = Backbone.View.extend({
+
+        template: Templates['modal'],
+
+        events: {
+
+        },
+
+        id: 'modal-view',
+
+        initialize: function() {
+
+        },
+
+        render: function() {
+
+            return this;
+        },
+
+        show: function() {
+
+        },
+
+        onModalHidden: function(e) {
+
+        }
+        
+    });
+});
