7336cd6d72088acc60fb32aaed02ae71b77e757b
Aaron Zemetres <azemetre@gmail.com>
2015-05-24 03:20:57 -0400

    add_place added to definitions; addNewPlace function allows adding additional cities to functionality

--- static/js/app/views/dash.js
+++ static/js/app/views/dash.js
@@ -3,8 +3,9 @@ define([
     'underscore',
     'backbone',
     'app/collections/places',
-    'app/views/place'
-], function ($, _, Backbone, PlacesCollection, PlaceView) {
+    'app/views/place',
+    'app/views/add_place'
+], function ($, _, Backbone, PlacesCollection, PlaceView, AddPlaceView) {
 
     'use strict';
 
@@ -60,7 +61,12 @@ define([
         },
 
         addNewPlace: function(e) {
-
+            var modal = new AddPlaceView({
+                title: 'Add a new place',
+                id: 'modal-add-new-place',
+                collection: this.collection
+            });
+            modal.show();
         },
 
         cleanUp: function() {
